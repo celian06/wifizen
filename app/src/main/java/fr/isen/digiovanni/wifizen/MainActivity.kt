704f97f1635030004ca13e838c904fff89da9630
Alreven <collard.florian@gmail.com>
2025-03-11 11:14:21 +0100

    flo

--- app/src/main/java/fr/isen/digiovanni/wifizen/MainActivity.kt
+++ app/src/main/java/fr/isen/digiovanni/wifizen/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
